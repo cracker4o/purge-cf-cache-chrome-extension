1fbe0449edce8bec214ecbb38d2f313992a3f70b
Tosho Toshev <tosho_az@yahoo.com>
2016-02-23 23:09:29 -0500

    Added some initial styling

---
+++ scripts/site.js
@@ -0,0 +1,13 @@
+(function() {
+    var elements = {
+        
+    };
+    
+    var operations = {
+        init: function () {
+            
+        }
+    };
+    
+    operations.init();
+})();
