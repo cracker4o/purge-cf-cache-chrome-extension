8259e12daff6fff3885d3a5d3056a4744dba248f
Tosho Toshev <tosho.toshev@emerson.com>
2016-11-01 13:13:17 -0400

    Initial implementation of the custom purge functionality

--- options.js
+++ options.js
@@ -27,6 +27,11 @@
                    e.preventDefault();
                    owner.saveClick();                
                });
+
+               $("#custom-purge").on("click", function(e) {
+                   e.preventDefault();
+                   owner.customPurgeClick();
+               })
                
                this.restoreOptions();
            },
@@ -57,6 +62,13 @@
                
                chrome.storage.sync.set(this.settings, function() {
                     $("#status").text("Options saved.");
+
+                    if($("#key").val() != "" && $("#email").val() != "") {
+                        $("#custom-purge-group").show();
+                    } else {
+                        $("#custom-purge-group").hide();
+                    }
+
                     setTimeout(function() {
                         $('#status').text("");
                     }, 1500);
@@ -67,18 +79,38 @@
                var url = $("#custom-url").val();
                var key = $("#key").val();
                var email = $("#email").val();
+               var domain = "";
 
-               if(url == "") {
+               if(url === "") {
                    return;
                }
 
-               var domain = cloudflareApi.helpers.getDomain(url);
-               if(domain == "") {
-                   return;
-               }
+               var urls = url.indexOf(',') >= 0 ? url.split(',') : [url];
+               
+               for(var i = 0; i < urls.length; i++) {
+                   urls[i] = urls[i].trim();
+                   var regex = new RegExp(/^https?:\/\/.*/g);
+                   if(urls[i].match(regex) == null) {
+                       options.setStatusMessage("#purge-status", "URL validation error - " + urls[i], 3000);
+                       return;
+                   }
+
+                    var tmpDomain = cloudflareApi.helpers.getDomain(urls[i]);
+                    if(tmpDomain === "") {
+                        options.setStatusMessage("#purge-status", "Could not determine the domain for url: " + urls[i], 3000);
+                        return;
+                    }
+
+                    if(domain === "") {
+                        domain = tmpDomain;
+                    } else if (domain !== tmpDomain) {
+                        options.setStatusMessage("#purge-status", "All urls should be under the same domain.", 3000);
+                        return;
+                    }
+               } 
 
                cloudflareApi.api.getZoneId(domain, email, key, function(zoneId) {
-                   cloudflareApi.api.purgeCache({ "files": [url] }, zoneId, email, key, function(purgeSuccessId) {
+                   cloudflareApi.api.purgeCache({ "files": urls }, zoneId, email, key, function(purgeSuccessId) {
                        options.setStatusMessage("#purge-status", "PURGE SUCESSFUL - Purge vector: " + purgeSuccessId, 3000)
                    }, function(errorMessage) {
                        options.setStatusMessage("#purge-status", errorMessage, 3000)
@@ -97,6 +129,12 @@
                        $("#refresh").val(items.refresh);    
                    } else {
                        $("#refresh").val(10);
+                   }
+
+                   if(items.key != null && items.key !== "" && items.email != null && items.email !== "") {
+                       $("#custom-purge-group").show();
+                   } else {
+                       $("#custom-purge-group").hide();
                    }
                    
                });
