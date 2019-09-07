195b1228d929a8ccc26d52ef21b428a384dd996a
Tosho <tosho_az@yahoo.com>
2019-09-06 22:41:19 -0700

    Added a new token-setup.html page

--- scripts/site.js
+++ scripts/site.js
@@ -1,11 +1,14 @@
 (function() {
     var elements = {
-        
+        yearElement: document.getElementById('year')
     };
     
     var operations = {
         init: function () {
-            
+            operations.setYear(elements.yearElement);
+        },
+        setYear: function (element) {
+            element.innerHTML = new Date().getFullYear();
         }
     };
     
